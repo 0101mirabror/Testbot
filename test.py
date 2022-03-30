950336de0098d9ff3e34e555698cd8c2e7f0497e
Mirabror Fayzullayev <fayzullayevmirabror@gmail.com>
2022-03-31 01:10:37 +0500

    last changes

---
+++ test.py
@@ -0,0 +1,33 @@
+# # # fullname = 'airabror aayzullayev'
+# # # print(fullname.split()[0][0].isupper())
+# # # def filter(message):
+# # #     return 'pyt' in message
+# # # print(filter('py4thon'))
+# # import xlsxwriter
+
+# from openpyxl import load_workbook
+
+# # file_path = 'test.xlsx'
+
+# # wb = load_workbook(file_path)
+
+# # ws = wb['SHEET_NAME']  # or wb.active
+
+# # ws['G6'] = 123
+
+# # wb.save(file_path)
+
+
+# from openpyxl import load_workbook
+# wb = load_workbook('test.xlsx')
+
+# work_sheet = wb.active # Get active sheet
+# work_sheet.append(['John1', 'Customer2', 'He likes football2'])
+ 
+# wb.save('test.xlsx')
+
+text = 'salom'
+if text:
+    print(True)
+else:
+    print("False")
